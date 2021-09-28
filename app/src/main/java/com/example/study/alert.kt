6fd501af1007b9758726c5b864e79db9dc8c7ac8
Faisal <falfariheedi@gmail.com>
2021-09-28 20:47:54 +0300

    edit

--- app/src/main/java/com/example/study/alert.kt
+++ app/src/main/java/com/example/study/alert.kt
@@ -3,12 +3,14 @@ package com.example.study
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.widget.TextView
 
 fun alert(mes:String ,toptitle:String,cont:Context){
     val dialogBuilder = AlertDialog.Builder(cont)
 
     // set message of alert dialog
     dialogBuilder.setMessage(mes)
+
         // if the dialog is cancelable
         .setCancelable(false)
     // positive button text and action
@@ -28,4 +30,6 @@ fun alert(mes:String ,toptitle:String,cont:Context){
 
     // show alert dialog
     alert.show()
+    val a: TextView = alert.findViewById(android.R.id.message)
+    a.setTextSize(14f)
 }
--- app/src/main/java/com/example/study/androidstudy.kt
+++ app/src/main/java/com/example/study/androidstudy.kt
@@ -13,7 +13,14 @@ class androidstudy : AppCompatActivity() {
         rv = findViewById(R.id.rv)
         var input = ArrayList<ArrayList<String>>()
         rv.layoutManager = LinearLayoutManager(this)
-        input.add(arrayListOf())
+        input.add(arrayListOf("main activity","main activity is automatically made by android studio","main activity is automatically made by android studio and looks like this\n" +
+                "class androidstudy : AppCompatActivity() {\n" +
+                "    override fun onCreate(savedInstanceState: Bundle?) {\n" +
+                "        super.onCreate(savedInstanceState)\n" +
+                "        setContentView(R.layout.activity_Main)\n" +
+                "   }\n" +
+                "}"))
+
         rv.adapter=RVAdapter(input,this)
 
         }
